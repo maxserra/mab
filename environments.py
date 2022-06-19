1dd90f17c965ac81b1c7a9bca19c54939bf677dd
Max Serra <mserralasierra@gmail.com>
2022-06-19 19:42:06 +0200

    implement adversaria environment

--- environments.py
+++ environments.py
@@ -11,3 +11,37 @@ class MultiArmedBanditEnvironment:
             return np.random.binomial(n=1, p=self.arm_probs[arm_i])
         else:
             raise ValueError(f"The given value '{arm_i}' is invalid. Must be positive and below '{len(self.arm_probs)}'")
+
+
+class AdversarialExp3Environment:
+
+    def __init__(self, arm_probs):
+        self.original_arm_probs = arm_probs
+        self.arm_probs = arm_probs
+
+        self.reward_tracking = [0 for _ in range(len(arm_probs))]
+
+    def _update_reward_tracking(self, arm_i, reward):
+        self.reward_tracking[arm_i] += reward
+
+    def _temper_with_arm_probs(self):
+        sorted_probs = np.flip(np.sort(self.original_arm_probs))
+        sorted_arms_by_reward = np.argsort(self.reward_tracking)
+
+        j = 0
+        for i in sorted_arms_by_reward:
+            self.arm_probs[i] = sorted_probs[j]
+            j += 1
+
+        print(f"Sorted probs {sorted_probs} | {sorted_arms_by_reward} -> {self.arm_probs}")
+
+    def take_action(self, arm_i):
+        if arm_i >= 0 and arm_i < len(self.arm_probs):
+            reward = np.random.binomial(n=1, p=self.arm_probs[arm_i])
+        else:
+            raise ValueError(f"The given value '{arm_i}' is invalid. Must be positive and below '{len(self.arm_probs)}'")
+
+        self._update_reward_tracking(arm_i, reward)
+        self._temper_with_arm_probs()
+
+        return reward
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import environments as envs
 import agents as ags
 
 
-def thomson_sampling(arm_probs, n_games, n_rounds):
+def thompson_sampling(arm_probs, n_games, n_rounds):
 
     env = envs.MultiArmedBanditEnvironment(arm_probs=arm_probs)
 
@@ -47,10 +47,60 @@ def exp3_stochastic_arms(arm_probs, n_games, n_rounds):
     print(np.mean(rewards_list))
 
 
+def exp3_adversarial_env(arm_probs, n_games, n_rounds):
+
+    rewards_list = []
+
+    for _ in tqdm(range(n_games)):
+
+        env = envs.AdversarialExp3Environment(arm_probs=arm_probs)
+        agent = ags.Exp3Agent(n_arms=len(arm_probs), gamma=0.07)
+
+        for _ in range(n_rounds):
+
+            print(f"Adverarial report: \n\t{env.reward_tracking} | {env.arm_probs}")
+
+            choosen_arm = agent.choose_action()
+            reward = env.take_action(arm_i=choosen_arm)
+            agent.update(arm_i=choosen_arm, reward=reward)
+
+        rewards_list.append(agent.reward)
+
+    print(np.mean(rewards_list))
+
+
+def thompson_adversarial_env(arm_probs, n_games, n_rounds):
+
+    rewards_list = []
+
+    for _ in tqdm(range(n_games)):
+
+        env = envs.AdversarialExp3Environment(arm_probs=arm_probs)
+        agent = ags.ThompsonAgent(n_arms=len(arm_probs))
+
+        for _ in range(n_rounds):
+
+            print(f"Adverarial report: \n\t{env.reward_tracking} | {env.original_arm_probs}")
+
+            choosen_arm = agent.choose_action()
+            reward = env.take_action(arm_i=choosen_arm)
+            agent.update(arm_i=choosen_arm, reward=reward)
+
+        rewards_list.append(agent.reward)
+
+    print(np.mean(rewards_list))
+
+
 if __name__ == '__main__':
 
-    # thomson_sampling(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
+    # thompson_sampling(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
     #                  n_games=30, n_rounds=1000)
 
-    exp3_stochastic_arms(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
-                         n_games=1, n_rounds=1000)
+    # exp3_stochastic_arms(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
+    #                      n_games=30, n_rounds=1000)
+
+    # exp3_adversarial_env(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
+    #                      n_games=10, n_rounds=1000)
+
+    thompson_adversarial_env(arm_probs=[0.3, 0.5, 0.4, 0.45, 0.3, 0.35],
+                             n_games=10, n_rounds=1000)
